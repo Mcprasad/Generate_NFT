83558367a295aca7da44daae8ba1451ccb77de69
Prasad Mecheri Chandravihar <prasadmecheri@gmail.com>
2022-01-09 19:06:55 -0600

    0.0.2 build

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Generate random mutations of an image for an NFT collection'
 
 # Setting up
